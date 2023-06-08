48eb2a2964d096b0bc21b55f1a1fc8adb1076846
Ahammed Rahees <ahdrahees.dev@gmail.com>
2023-06-08 15:55:51 +0530

    Modifief

--- contracts/BasicNft.sol
+++ contracts/BasicNft.sol
@@ -4,8 +4,10 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 pragma solidity 0.8.18;
 
 contract BasicNft is ERC721 {
+    string public constant TOKEN_URI = "ipfs://bafkreic5jpc7iod6t42xuct54x46q56wmp7ymvsgx4xyktiizuudxa2ova/"; 
     uint256 private s_tokenCounter;
     // constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {} // Or we can directly pass args
+    
     constructor() ERC721("Rindappan","RIN"){
         s_tokenCounter = 0;
     };
@@ -15,6 +17,11 @@ contract BasicNft is ERC721 {
         s_tokenCounter = s_tokenCounter +1;
         return s_tokenCounter;
     };
+    function tokenURI(uint256 /* tokenId */) public view override returns (string memory) {
+        return TOKEN_URI;
+    }
+
+
     function getTokenCounter() public view  returns (uint256) {
         return s_tokenCounter;
     }
