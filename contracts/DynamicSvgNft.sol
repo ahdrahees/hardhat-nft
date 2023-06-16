f684f32cc55c551e5568f0ab72cfda0906e6e29e
Ahammed Rahees <ahdrahees.dev@gmail.com>
2023-06-16 22:18:19 +0530

    Added more get functions

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -4,14 +4,15 @@ pragma solidity 0.8.18;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "base64-sol/base64.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DynamicSvgNft is ERC721 {
+contract DynamicSvgNft is ERC721 /*, Ownable */ {
     // mint
     // store svg some where
     // some logic to say show x image or show y image
     uint256 private s_tokenCounter;
-    string private i_highImageURI;
-    string private i_lowImageURI;
+    string private s_highImageURI;
+    string private s_lowImageURI;
     string private constant base64EncodedSvgPrefix = "data:image/svg+xml;base64,";
 
     AggregatorV3Interface internal immutable i_dataFeed;
@@ -26,8 +27,8 @@ contract DynamicSvgNft is ERC721 {
         string memory highSvg
     ) ERC721("Dynamic SVG NFT", "DSN") {
         s_tokenCounter = 0;
-        i_lowImageURI = svgToImageURI(lowSvg);
-        i_highImageURI = svgToImageURI(highSvg);
+        s_lowImageURI = svgToImageURI(lowSvg);
+        s_highImageURI = svgToImageURI(highSvg);
         i_dataFeed = AggregatorV3Interface(priceFeedAddress);
         // 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43 BTC/USD price feed sepolia
     }
@@ -42,7 +43,6 @@ contract DynamicSvgNft is ERC721 {
     function mintNft(int256 highValue) public {
         s_tokenIdToHighValue[s_tokenCounter] = highValue;
         _safeMint(msg.sender, s_tokenCounter);
-
         emit CreatedNFT(s_tokenCounter, highValue);
         s_tokenCounter += 1;
     }
@@ -61,9 +61,9 @@ contract DynamicSvgNft is ERC721 {
 
         // prefix for json base64 is  data:application/json;base64,
         (, int256 price, , , ) = i_dataFeed.latestRoundData();
-        string memory imageURI = i_lowImageURI;
+        string memory imageURI = s_lowImageURI;
         if (price >= s_tokenIdToHighValue[tokenId]) {
-            imageURI = i_highImageURI;
+            imageURI = s_highImageURI;
         }
         // for example:- when user minting this Nft he set price high price is 30000 usd for btc. if the BTC price is equal or above high price then tokenURI dynamically changes according to price of BTC. when we call tokenURI IT will show new image. if price go below high price it will change tokenURI.
         // here we created this condition for tokenURI() to return tokenURI of the NFT. this make it dynamically changing
@@ -86,6 +86,40 @@ contract DynamicSvgNft is ERC721 {
                     )
                 )
             );
+    }
+
+    // function setLowURI(string memory svgLowURI) public onlyOwner {
+    //     s_lowImageURI = svgLowURI;
+    // }
+
+    // function setHighURI(string memory svgHighURI) public onlyOwner {
+    //     s_highImageURI = svgHighURI;
+    // }
+
+    // function setLowSVG(string memory svgLowRaw) public onlyOwner {
+    //     string memory svgURI = svgToImageURI(svgLowRaw);
+    //     setLowURI(svgURI);
+    // }
+
+    // function setHighSVG(string memory svgHighRaw) public onlyOwner {
+    //     string memory svgURI = svgToImageURI(svgHighRaw);
+    //     setHighURI(svgURI);
+    // }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
+
+    function getHighSvgImageURI() public view returns (string memory) {
+        return s_highImageURI;
+    }
+
+    function getLowSvgImageURI() public view returns (string memory) {
+        return s_lowImageURI;
+    }
+
+    function getPriceFeed() public view returns (AggregatorV3Interface) {
+        return i_dataFeed;
     }
 }
 
